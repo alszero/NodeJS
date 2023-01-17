f42f92e3108717dfef927684e074ff1d02d53778
alszero <alszero05@gmail.com>
2023-01-17 12:16:47 +0900

    문자열 길이 구하기

--- web2-nodejs/syntax/string.js
+++ web2-nodejs/syntax/string.js
@@ -1,4 +1,5 @@
 //문자열 데이터의 덧셈 연산
 console.log(1 + 1); //2
 console.log('1' + '1'); //11
+console.log('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'.length);
 //두번째 줄의 결과가 11이 나온 이유는 숫자면 두 수를 더하고, 문자열이면 두 문자열을 결합하기 때문
