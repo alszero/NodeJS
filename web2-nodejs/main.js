5af759862d2032750a5fc59304c239bf59d3f5c0
alszero <alszero05@gmail.com>
2023-01-18 11:51:15 +0900

    쿼리 스트링에서 입력받은 id 값을 웹 페이지로 출력

--- web2-nodejs/main.js
+++ web2-nodejs/main.js
@@ -1,17 +1,20 @@
 //Node.js를 웹 서버로 만들어주는 코드
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
 var app = http.createServer(function(request, response){
-    var url = request.url;
-    if(request.url == '/'){
-        url = '/index.html';
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    console.log(queryData.id);
+    if(_url == '/'){
+        _url = '/index.html';
     }
-    if(request.url == '/favicon.ico'){
+    if(_url == '/favicon.ico'){
         return response.writeHead(404);
     }
     response.writeHead(200);
-    console.log(__dirname +url);//웹 브라우저가 요청한 파일의 경로를 콘솔에 출력하는 명령 추가
-    response.end(fs.readFileSync('__dirname' + url));
+    // console.log(__dirname +url);//웹 브라우저가 요청한 파일의 경로를 콘솔에 출력하는 명령 추가
+    response.end(queryData.id);
     //response.end()는 웹 서버가 웹 브라우저의 요청에 응답하는 명령
 });
 app.listen(3000);
