d2b8d566fb813e483c053529702e79fe53f779e9
alszero <alszero05@gmail.com>
2023-01-18 09:40:59 +0900

    변수를 사용하지 않았을 때

---
+++ web2-nodejs/syntax/variable2.js
@@ -0,0 +1,2 @@
+// 변수를 사용하지 않았을 때
+console.log('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
